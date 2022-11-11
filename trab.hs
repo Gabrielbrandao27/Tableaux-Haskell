10a432c3064954431c3910f6dfd4043e7089351b
Gabriel Brandão <gabriel.brandao@hashdex.com>
2022-11-11 14:20:13 -0300

    adding ajusta e operador functions

--- trab.hs
+++ trab.hs
@@ -1,2 +1,13 @@
-arvore :: (Char, Char) -> (Char, Char)
-arvore (origem, formula)
+--formula = ("aVb", "V", "a^b")
+str = "(aVb)V(a^b)"
+
+ajeita :: String -> ([Char], Char, [Char])
+ajeita str = (prim, op, seg)
+    where prim = take 5 str
+          op = last (take 6 str)
+          seg = reverse (take 5 (reverse str))
+
+
+
+operador ::(a, b, c) -> b
+operador (_,b,_) = b
